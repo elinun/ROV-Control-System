a0868ff241376a488acefbaee1dc27e45bd11aa7
elinun <33405991+elinun@users.noreply.github.com>
2018-04-16 00:50:51 -0400

    Major Overhaul

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ConnectBtn = new System.Windows.Forms.Button();
+            this.debug = new System.Windows.Forms.TextBox();
+            this.arduinoIP = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.vScrollBar2 = new System.Windows.Forms.VScrollBar();
+            this.vScrollBar3 = new System.Windows.Forms.VScrollBar();
+            this.vScrollBar4 = new System.Windows.Forms.VScrollBar();
+            this.vScrollBar5 = new System.Windows.Forms.VScrollBar();
+            this.vScrollBar6 = new System.Windows.Forms.VScrollBar();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.SuspendLayout();
+            // 
+            // ConnectBtn
+            // 
+            this.ConnectBtn.Location = new System.Drawing.Point(127, 12);
+            this.ConnectBtn.Name = "ConnectBtn";
+            this.ConnectBtn.Size = new System.Drawing.Size(75, 23);
+            this.ConnectBtn.TabIndex = 1;
+            this.ConnectBtn.Text = "Connect";
+            this.ConnectBtn.UseVisualStyleBackColor = true;
+            this.ConnectBtn.Click += new System.EventHandler(this.ConnectBtn_Click);
+            // 
+            // debug
+            // 
+            this.debug.Location = new System.Drawing.Point(32, 41);
+            this.debug.Name = "debug";
+            this.debug.Size = new System.Drawing.Size(170, 20);
+            this.debug.TabIndex = 2;
+            // 
+            // arduinoIP
+            // 
+            this.arduinoIP.Location = new System.Drawing.Point(12, 12);
+            this.arduinoIP.Name = "arduinoIP";
+            this.arduinoIP.Size = new System.Drawing.Size(100, 20);
+            this.arduinoIP.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer
+            // 
+            this.timer.AutoSize = true;
+            this.timer.Location = new System.Drawing.Point(29, 80);
+            this.timer.Name = "timer";
+            this.timer.Size = new System.Drawing.Size(34, 13);
+            this.timer.TabIndex = 4;
+            this.timer.Text = "00:00";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 75);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(150, 75);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(32, 141);
+            this.vScrollBar1.Maximum = 99;
+            this.vScrollBar1.Minimum = -99;
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(17, 80);
+            this.vScrollBar1.TabIndex = 7;
+            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar_Scroll);
+            // 
+            // vScrollBar2
+            // 
+            this.vScrollBar2.Location = new System.Drawing.Point(69, 141);
+            this.vScrollBar2.Maximum = 99;
+            this.vScrollBar2.Minimum = -99;
+            this.vScrollBar2.Name = "vScrollBar2";
+            this.vScrollBar2.Size = new System.Drawing.Size(17, 80);
+            this.vScrollBar2.TabIndex = 8;
+            this.vScrollBar2.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar_Scroll);
+            // 
+            // vScrollBar3
+            // 
+            this.vScrollBar3.Location = new System.Drawing.Point(107, 141);
+            this.vScrollBar3.Maximum = 99;
+            this.vScrollBar3.Minimum = -99;
+            this.vScrollBar3.Name = "vScrollBar3";
+            this.vScrollBar3.Size = new System.Drawing.Size(17, 80);
+            this.vScrollBar3.TabIndex = 9;
+            this.vScrollBar3.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar_Scroll);
+            // 
+            // vScrollBar4
+            // 
+            this.vScrollBar4.Location = new System.Drawing.Point(150, 141);
+            this.vScrollBar4.Maximum = 99;
+            this.vScrollBar4.Minimum = -99;
+            this.vScrollBar4.Name = "vScrollBar4";
+            this.vScrollBar4.Size = new System.Drawing.Size(17, 80);
+            this.vScrollBar4.TabIndex = 10;
+            this.vScrollBar4.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar_Scroll);
+            // 
+            // vScrollBar5
+            // 
+            this.vScrollBar5.Location = new System.Drawing.Point(185, 141);
+            this.vScrollBar5.Maximum = 99;
+            this.vScrollBar5.Minimum = -99;
+            this.vScrollBar5.Name = "vScrollBar5";
+            this.vScrollBar5.Size = new System.Drawing.Size(17, 80);
+            this.vScrollBar5.TabIndex = 11;
+            this.vScrollBar5.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar_Scroll);
+            // 
+            // vScrollBar6
+            // 
+            this.vScrollBar6.Location = new System.Drawing.Point(225, 141);
+            this.vScrollBar6.Maximum = 99;
+            this.vScrollBar6.Minimum = -99;
+            this.vScrollBar6.Name = "vScrollBar6";
+            this.vScrollBar6.Size = new System.Drawing.Size(17, 80);
+            this.vScrollBar6.TabIndex = 12;
+            this.vScrollBar6.Scroll += new System.Windows.Forms.ScrollEventHandler(this.vScrollBar_Scroll);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(150, 257);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(238, 257);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Load";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 292);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.vScrollBar6);
+            this.Controls.Add(this.vScrollBar5);
+            this.Controls.Add(this.vScrollBar4);
+            this.Controls.Add(this.vScrollBar3);
+            this.Controls.Add(this.vScrollBar2);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.timer);
+            this.Controls.Add(this.arduinoIP);
+            this.Controls.Add(this.debug);
+            this.Controls.Add(this.ConnectBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button ConnectBtn;
+        private System.Windows.Forms.TextBox debug;
+        private System.Windows.Forms.TextBox arduinoIP;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label timer;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.VScrollBar vScrollBar2;
+        private System.Windows.Forms.VScrollBar vScrollBar3;
+        private System.Windows.Forms.VScrollBar vScrollBar4;
+        private System.Windows.Forms.VScrollBar vScrollBar5;
+        private System.Windows.Forms.VScrollBar vScrollBar6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,355 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO;
+using SlimDX.DirectInput;
+using System.Net.Sockets;
+using System.Linq;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        TcpClient tcpclnt;
+        Stream stream;
+        DirectInput input = new DirectInput();
+        SlimDX.DirectInput.Joystick stick;
+        static Joystick[] sticks;
+        Thread pollThread;
+        int x = 0;
+        int y = 0;
+        int z = 0;
+        char divider = '\t';
+
+        public void poll()
+        {
+            while (true)
+            {
+                if (stick != null)
+                {
+                    stickHandle(stick, 0);
+                }
+                else
+                {
+                    sticks = GetSticks();
+                }
+                Thread.Sleep(15);
+            }
+        }
+
+        public Joystick[] GetSticks()
+        {
+            List<SlimDX.DirectInput.Joystick> sticks = new List<SlimDX.DirectInput.Joystick>();
+           // List<Microsoft.DirectX.DirectInput.Joystick> mSticks = new List<Microsoft.DirectX.DirectInput.Joystick>();
+            foreach (DeviceInstance device in input.GetDevices(DeviceClass.GameController, DeviceEnumerationFlags.AttachedOnly))
+            {
+                try
+                {
+                    stick = new SlimDX.DirectInput.Joystick(input, device.InstanceGuid);
+                    stick.Acquire();
+                    sticks.Add(stick);
+                    Invoke(new MethodInvoker(delegate
+                    {
+                        debug.Text = "Controller Connected.";
+                        /*foreach (bool btn in buttons) {
+                            controlOut.Text += (btn ? "t " : "f ") ;
+                        }*/
+
+                    }));
+                }
+                catch (Exception e) { debug.Text += e.ToString(); }
+            }
+            return sticks.ToArray();
+        }
+
+        int lowDeadBand = 1100;//really min
+        int highDeadBand = 1900;//really max
+        int servoPos = 90;//temporary
+
+        void stickHandle(Joystick stick, int id)
+        {
+            JoystickState state = new JoystickState();
+            state = stick.GetCurrentState();
+            x = state.X;
+            y = state.Y;
+            z = state.Z;
+            int zPwr = (z / 82)+1100;//these are NOT arbitrarily created numbers
+            int xPwr = (x / 82)+1100;
+            int yPwr = (y / 82)+1100;
+            int rightTrig = (state.RotationY / 164) + 1500;
+            int leftTrig = 1500-(state.RotationX / 164);
+            bool[] buttons = state.GetButtons();
+
+            Invoke(new MethodInvoker(delegate
+                {
+                    debug.Text = "X: "+xPwr;                   
+                }));
+                
+                if (tcpclnt != null)
+                {
+                    if (tcpclnt.Connected)
+                    {
+                    String str = ((yPwr > 1550 && yPwr<highDeadBand)? divider+"f" + yPwr : "") + (yPwr < 1450 && yPwr>lowDeadBand ? divider+"f" + yPwr : "") +
+                        (xPwr > 1550 && xPwr <highDeadBand ? divider+"l" + xPwr : "") + (xPwr < 1450 && xPwr>lowDeadBand ? divider+"l" + xPwr : "") +
+                        (buttons[3] ? divider+"c0001" : "") + (buttons[1] ? divider+"v0001" : "") +
+                        (buttons[10] ? divider+"r0000":"")+
+                        (buttons[4] ? divider+"x0100":"")+(buttons[5] ? divider+"x-100":"")+
+                        (buttons[2] ? divider+",0001" : "") + (buttons[0] ? divider+".0001" : "") +//servo, adding this many degrees to position
+                        (zPwr > 1550 || zPwr < 1450 ? divider+"z" + zPwr : "")  +
+                        checkForBuzzer(buttons[13])+
+                        (rightTrig > 1501 ? divider+"h" + rightTrig : "") + (leftTrig < 1499 ? divider+"h" + leftTrig : "");
+                        str += checkForStop(xPwr, yPwr, zPwr);
+                    if (str.Length > 0)
+                    {
+                        
+                        str+=divider+"\n";
+                        ASCIIEncoding asen = new ASCIIEncoding();
+                        Invoke(new MethodInvoker(delegate
+                        {
+                            //debug.Text = asen.GetString(asen.GetBytes(str));
+                        }));
+                        byte[] ba = asen.GetBytes(str);
+                        stream.Write(ba, 0, ba.Length);
+                    }
+                       
+                    }
+                
+            }
+           
+        }
+        string tempClawControl(string cmd)
+        {
+            return cmd;
+        }
+
+        bool xStopped = true;
+        bool yStopped = true;
+        bool zStopped = true;
+        bool buzzerOn = false;
+
+        string checkForBuzzer(bool button)
+        {
+            string retVal = "";
+            if (!buzzerOn && button)
+            {
+                retVal += divider+"a0000";
+                buzzerOn = true;
+            }
+            else if (buzzerOn && !button)
+            {
+                buzzerOn = false;
+                retVal += divider + "a0000";
+            }
+
+                return retVal;
+        }
+
+        string checkForStop(int x, int y, int z)
+        {
+            string retVal = "";
+            if(x<1525 && x > 1475 && !xStopped)
+            {
+                retVal += divider+"l1500";
+            }
+            xStopped = !(x > 1525 || x < 1475);
+
+            if (y < 1525 && y > 1475 && !yStopped)
+            {
+                retVal += divider + "f1500";
+            }
+            yStopped = !(y > 1525 || y < 1475);
+
+            if (z < 1525 && z > 1475 && !zStopped)
+            {
+                retVal += divider + "z1500";
+            }
+            zStopped = !(z > 1525 || z < 1475);
+            return retVal;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            //GetSticks();
+            sticks = GetSticks();
+            pollThread = new Thread(poll);
+            pollThread.Start();
+
+            this.FormClosing += monClosing;
+            ConnectBtn.Click += ConnectBtn_Click;
+           // this.MouseMove += mouseMove;
+        }
+
+          
+
+            public void monClosing(object ev, FormClosingEventArgs e)// a better cleanup
+        {
+            if (pollThread.IsAlive)
+            {
+                //stick.Dispose();
+                pollThread.Abort();
+            }
+            if(tcpclnt != null)
+            {
+                tcpclnt.Close();
+            }
+        }
+
+        private void ConnectBtn_Click(object sender, EventArgs e)
+        {
+            debug.Text = "Connected to ROV";
+            if (tcpclnt != null)
+            {
+                tcpclnt.Close();
+            }
+            try
+            {
+                tcpclnt = new TcpClient();
+                tcpclnt.SendTimeout = 500;
+
+                tcpclnt.Connect(arduinoIP.Text, 1740);
+                stream = tcpclnt.GetStream();
+                stream.WriteTimeout = 500;
+            }
+            catch(Exception ex) { debug.Text = "Network Error"; }
+        }
+
+
+        int seconds = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            seconds++;
+            timer.Text = addZeros(seconds/60, 2)+":"+addZeros(seconds%60, 2);
+        }
+
+        string addZeros(int num, int numOfDigits)
+        {
+            string ret = "";
+            for(int i = numOfDigits-1; i>-1; i--)
+            {
+                if ((num / (int)(Math.Pow(10, i))) == 0)
+                {
+                    ret += "0";
+                }
+                else
+                {
+                    ret += num.ToString();
+                    i = -1;
+                }
+            }
+            return ret;
+        }
+
+        private void button1_Click(object sender, EventArgs e)//Start/Stop
+        {
+            if (timer1.Enabled)
+            {
+                button1.Text = "Start";
+                timer1.Stop();
+            }
+            else
+            {
+                button1.Text = "Stop";
+                timer1.Interval = 1000;
+                timer1.Start();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)//reset
+        {
+            seconds = 0;
+            timer.Text = "00:00";
+        }
+
+        private void vScrollBar_Scroll(object sender, ScrollEventArgs e)
+        {
+            string name = ((ScrollBar)sender).Name;
+            string str = "";
+            if (tcpclnt == null)
+            {
+                if ( stream == null)
+                {
+                    str = divider+"t" + makeTrimValue(e.NewValue) + name.Substring(name.Length - 1)+divider;
+                   
+                   
+                    ASCIIEncoding asen = new ASCIIEncoding();
+                    Invoke(new MethodInvoker(delegate
+                    {
+                        debug.Text = asen.GetString(asen.GetBytes(str));
+                    }));
+                    byte[] ba = asen.GetBytes(str);
+                    //stream.Write(ba, 0, ba.Length);
+                }
+            }
+        }
+
+        string makeTrimValue(int pwr)
+        {
+            string ret = "";
+            if (pwr > 0)
+            {
+                ret += addZeros(pwr, 3);
+            }
+            else
+            {
+                ret += "-";
+                ret += addZeros(Math.Abs(pwr), 2);
+            }
+            return ret;
+        }
+
+        bool saving = false;
+        private void button3_Click(object sender, EventArgs e)//save
+        {
+            saving = true;
+            saveFileDialog1.Filter = "Text File|*.txt";
+            saveFileDialog1.FileName = "trims.txt";
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)//load
+        {
+            saving = false;
+            saveFileDialog1.OverwritePrompt = false;
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (saving)
+            {
+                string str = "";
+                ScrollBar bar;
+                for (int i = 1; i < 7;i++)
+                {
+                    bar = this.Controls.Find("vScrollBar"+i, true).FirstOrDefault() as ScrollBar;
+                    str += bar.Value+",";
+                    
+                }
+                str = str.Substring(0, str.Length - 1);
+                using (StreamWriter sw = new StreamWriter(saveFileDialog1.FileName))
+                    sw.WriteLine(str);
+
+
+            }
+            else
+            {
+                String trims;
+                using (StreamReader sr = new StreamReader(saveFileDialog1.FileName))
+                    trims = sr.ReadLine();
+                string[] sValues = trims.Split(',');
+                ScrollBar bar;
+                for(int i = 1; i < 7; i++)
+                {
+                    bar = this.Controls.Find("vScrollBar" + i, true).FirstOrDefault() as ScrollBar;
+                    bar.Value = int.Parse(sValues[i - 1]);
+                    vScrollBar_Scroll((object)bar, new ScrollEventArgs(ScrollEventType.SmallIncrement, bar.Value));
+                }
+            }
+        }
+
+ 
+    }
+}
